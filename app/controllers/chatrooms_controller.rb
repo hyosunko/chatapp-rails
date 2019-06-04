11879570bd709e3b4a1b1d651afc8af09c0a3308
hyosunko <hyosunko@gmail.com>
2019-06-04 12:02:31 -0700

    add chatroom create method, view, and user link

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -5,4 +5,26 @@ class ChatroomsController < ApplicationController
     @users = User.all.where.not(id: current_user)
     @chatrooms = Chatroom.includes(:recipient, :messages).find(session[:chatrooms])
   end
+
+  def create
+    @chatroom = Chatroom.get(current_user.id, params[:user_id])
+
+    add_to_chatrooms unless conversated?
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+
+  def add_to_chatrooms
+    session[:chatrooms] ||= []
+    session[:chatrooms] << @chatroom.id
+  end
+
+  def conversated?
+    session[:chatrooms].include?(@chatroom.id)
+  end
+
 end
