7911d4878df4508682c9b9c1bb7921121e812484
hyosunko <hyosunko@gmail.com>
2019-06-04 13:37:47 -0700

    create initial speak and received mehtods

--- app/assets/javascripts/channels/chatroom.js
+++ app/assets/javascripts/channels/chatroom.js
@@ -1,17 +1,18 @@
 App.chatroom = App.cable.subscriptions.create("ChatroomChannel", {
-  connected: function() {
-    // Called when the subscription is ready for use on the server
-  },
-
-  disconnected: function() {
-    // Called when the subscription has been terminated by the server
-  },
-
-  received: function(data) {
-    // Called when there's incoming data on the websocket for this channel
-  },
-
-  speak: function() {
-    return this.perform('speak');
-  }
+    connected: function() {},
+    disconnected: function() {},
+    received: function(data) {
+        console.log(data["message"]);
+    },
+    speak: function(message) {
+        return this.perform("speak", {
+            message: message
+        });
+    }
+});
+$(document).on("submit", ".new_message", function(e) {
+    e.preventDefault();
+    var values = $(this).serializeArray();
+    App.chatroom.speak(values);
+    $(this).trigger("reset");
 });
--- app/channels/chatroom_channel.rb
+++ app/channels/chatroom_channel.rb
@@ -1,12 +1,20 @@
 class ChatroomChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "chatrooms-#{current_user.id}"
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    stop_all_streams
   end
 
-  def speak
+  def speak(data)
+    message_params = data['message'].each_with_object({}) do |el, hash|
+      hash[el.values.first] = el.values.last
+    end
+
+    ActionCable.server.broadcast(
+      "chatroom-#{current_user.id}",
+      message: message_params
+    )
   end
 end
