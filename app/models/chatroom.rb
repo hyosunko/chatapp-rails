e0728ce8ce8e0ea53b5e1deeea47bc4941234fbf
hyosunko <hyosunko@gmail.com>
2019-06-04 11:58:30 -0700

    add chatroom create route and between scope and get method

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -5,6 +5,19 @@ class Chatroom < ApplicationRecord
 
   validates :sender_id, uniqueness: { scope: :recipient_id }
 
+  scope :between, -> (sender_id, recipient_id) do
+    where(sender_id: sender_id, recipient_id: recipient_id).or(
+      where(sender_id: recipient_id, recipient_id: sender_id)
+    )
+  end
+
+  def self.get(sender_id, recipient_id)
+    chatroom = between(sender_id, recipient_id).first
+    return chatroom if chatroom.present?
+
+    create(sender_id: sender_id, recipient_id: recipient_id)
+  end
+
   def opposed_user(user)
     user == recipient ? sender : recipient
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   get '/chatrooms', to: 'chatrooms#index'
+
+  resources :chatrooms, only: [:create]
 end
