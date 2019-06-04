d77d79cee2a3027033bde363dec7743c85b4f933
hyosunko <hyosunko@gmail.com>
2019-06-04 13:47:44 -0700

    add broadcasting job method, and modify receiving method in client side

--- app/assets/javascripts/channels/chatroom.js
+++ app/assets/javascripts/channels/chatroom.js
@@ -2,7 +2,17 @@ App.chatroom = App.cable.subscriptions.create("ChatroomChannel", {
     connected: function() {},
     disconnected: function() {},
     received: function(data) {
-        console.log(data["message"]);
+        var chatroom = $("#chatrooms-list").find(
+            "[data-chatroom-id='" + data["chatroom_id"] + "']"
+        );
+        chatroom
+            .find(".messages-list")
+            .find("ul")
+            .append(data["message"]);
+
+        var messages_list = chatroom.find(".messages-list");
+        var height = messages_list[0].scrollHeight;
+        messages_list.scrollTop(height);
     },
     speak: function(message) {
         return this.perform("speak", {
--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -1,7 +1,36 @@
 class MessageBroadcastJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(message)
+    sender = message.user
+    recipient = message.chatroom.opposed_user(sender)
+
+    broadcast_to_sender(sender, message)
+    broadcast_to_recipient(recipient, message)
+  end
+
+  private
+
+  def broadcast_to_sender(user, message)
+    ActionCable.server.broadcast(
+      "chatrooms-#{user.id}",
+      message: render_message(message, user),
+      chatroom_id: message.chatroom_id
+    )
+  end
+
+  def broadcast_to_recipient(user, message)
+    ActionCable.server.broadcast(
+      "chatrooms-#{user.id}",
+      message: render_message(message, user),
+      chatroom_id: message.chatroom_id
+    )
+  end
+
+  def render_message(message, user)
+    ApplicationController.render(
+      partial: 'messages/message',
+      locals: { message: message, user: user }
+    )
   end
 end
